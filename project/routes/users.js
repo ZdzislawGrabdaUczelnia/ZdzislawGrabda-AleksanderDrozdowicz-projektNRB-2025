037a36d6a4c232ab0227d17b627cef53f395d615
Zdzisław Grabda S22-31 <upmalochadneing@protonmail.com>
2025-01-23 10:24:47 +0100

    Update users.js

--- project/routes/users.js
+++ project/routes/users.js
@@ -4,7 +4,26 @@ const authenticateToken = require('../middleware/auth');
 const router = express.Router();
 
 
+router.get('/', authenticateToken, async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: 'Błąd pobierania użytkowników' });
+  }
+});
 
+router.get('/:id', authenticateToken, async (req, res) => {
+  try {
+    const user = await User.findOne({ user_id: req.params.id });
+    if (!user) return res.status(404).json({ error: 'Użytkownik nie znaleziony' });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: 'Błąd pobierania użytkownika' });
+  }
+});
+
+/*
 router.get('/', authenticateToken, async (req, res) => {
   try {
     const populate = req.query.populate === 'true';  
@@ -26,25 +45,6 @@ router.get('/', authenticateToken, async (req, res) => {
     res.status(500).json({ error: 'Błąd pobierania użytkowników' });
   }
 });
-
-
-
-router.get('/:id', authenticateToken, async (req, res) => {
-  try {
-    const user = await User.findOne({ user_id: req.params.id });
-
-    if (!user) {
-      return res.status(404).json({ error: 'Użytkownik nie znaleziony' });
-    }
-
-    
-    console.log('User found:', user);  
-
-    res.json(user);  
-  } catch (err) {
-    console.error('Error fetching user by ID:', err);  
-    res.status(500).json({ error: 'Błąd pobierania użytkownika' });
-  }
-});
+*/
 
 module.exports = router;
